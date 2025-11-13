b32347e9b17ae1ce66b2e3e92b0a45087a77873a
Victor <victor.fox.hz@gmail.com>
2025-11-13 16:38:56 +0100

    added websockets for score

--- Backend/src/main/java/se/bytebase/vr_server/facade/ScoreFacade.java
+++ Backend/src/main/java/se/bytebase/vr_server/facade/ScoreFacade.java
@@ -1,20 +1,25 @@
 package se.bytebase.vr_server.facade;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import se.bytebase.vr_server.dto.ScoreRequest;
 import se.bytebase.vr_server.model.ScoreModel;
 import se.bytebase.vr_server.repository.ScoreRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class ScoreFacade {
-    public static boolean addScore(String username, int score, String time, ScoreRepository scoreRepository) {
-        ScoreModel scoreModel = new ScoreModel(
-                username, score, time
-        );
-        scoreRepository.save(scoreModel);
-        return true;
+
+    public static ScoreModel addScore(String username, int score, String time, ScoreRepository repo) {
+        // Validation logic
+        if (username == null || username.isEmpty()) return null;
+
+        ScoreModel scoreModel = new ScoreModel(username, score, time);
+        return repo.save(scoreModel);
+    }
+
+    public static List<ScoreModel> getAllScores(ScoreRepository repo) {
+        return repo.findAll();
+    }
+
+    public static List<ScoreModel> getScoresByUser(String username, ScoreRepository repo) {
+        return repo.findByUsername(username);
     }
 }
